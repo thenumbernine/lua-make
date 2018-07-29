efc41328b7e3c900317daaf137ea7b62ea611f75
Christopher Moore <christopher.e.moore@gmail.com>
2018-07-29 13:35:29 +0200

    added separate distclean for lua -l

--- distclean.lua
+++ distclean.lua
@@ -2,9 +2,9 @@
 local table = require 'ext.table'
 local luabin = arg[table(arg):keys():sort():inf()]
 if os.getenv'HOME' then
-	os.execute(luabin..' '..os.getenv'HOME'..'/Projects/lua/make/run.lua clean')
+	os.execute(luabin..' '..os.getenv'HOME'..'/Projects/lua/make/run.lua distclean')
 elseif os.getenv'USERPROFILE' then
-	os.execute(luabin..' '..os.getenv'USERPROFILE'..[[\Projects\lua\make\run.lua clean]])
+	os.execute(luabin..' '..os.getenv'USERPROFILE'..[[\Projects\lua\make\run.lua distclean]])
 else
 	error "couldn't deduce platform"
 end
