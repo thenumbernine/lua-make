9ae62d67402e581763eff31952b7a02689649f4e
Christopher Moore <christopher.e.moore@gmail.com>
2022-10-20 11:36:10 +0700

    put the msvc detect first on windows

--- detect.lua
+++ detect.lua
@@ -9,23 +9,23 @@ local function detect()
 		-- TODO clang_osx vs gcc_osx?
 		return 'osx'
 	else
+		-- cl.exe has no version flag (I think), but outputs the version as default.
+		-- However it does so to stderr, which will get piped to nul.
+		-- However however it at least prints out its warning to stdout: "usage: cl ..."  hmm... seems it has mixed up the use of stdout and stderr...
+		if #io.popen'cl.exe 2> nul':read'a' > 0 then
+			-- MSVC
+			return 'msvc'
 		-- Windows always returns a string, it's just an empty string for .exe's that don't exist.
 		-- is this the same behavior on linux/osx?
 		-- Also, windows always prints an error message when the process doesn't exist .. to stderr.
-		-- Piping stderr to nul eliminates this.  
+		-- Piping stderr to nul eliminates this.
 		-- But it will also eliminate the output of a legitimate process.
-		if #io.popen'clang-cl.exe --version 2> nul':read'a' > 0 then
+		elseif #io.popen'clang-cl.exe --version 2> nul':read'a' > 0 then
 			-- CLang
 			return 'clang_win'
 		elseif #io.popen'gcc.exe --version 2> nul':read'a' > 0 then
 			-- GCC
 			return 'mingw'	-- TODO rename to gcc_win? distinguish between mingw_gcc, cygwin_gcc, djgpp_gcc, etc?
-		-- cl.exe has no version flag (I think), but outputs the version as default.
-		-- However it does so to stderr, which will get piped to nul.
-		-- However however it at least prints out its warning to stdout: "usage: cl ..."  hmm... seems it has mixed up the use of stdout and stderr...
-		elseif #io.popen'cl.exe 2> nul':read'a' > 0 then
-			-- MSVC
-			return 'msvc'
 		end
 	end
 end
